577a6924afa17f607e4e1df836946c5083089b2d
leleker <eswara.psr@gmail.com>
2022-07-09 14:29:09 +0530

    test

---
+++ src/main/java/com/example/firstspringbootawsdeployment/FirstSpringBootAwsDeploymentApplication.java
@@ -0,0 +1,14 @@
+package com.example.firstspringbootawsdeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootAwsDeploymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringBootAwsDeploymentApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/firstspringbootawsdeployment/FirstSpringBootAwsDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstspringbootawsdeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootAwsDeploymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
